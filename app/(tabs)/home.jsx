72484701dedb53852af99420cc88b706556069d8
James Othniel <jamesothniel7@gmail.com>
2024-06-27 03:17:50 +0100

    other added files by defult

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Header from "../../components/Home/Header";
+
+export default function home() {
+  return (
+    <View>
+      {/* Header */}
+      <Header />
+
+      {/* Slider */}
+
+      {/* Category */}
+
+      {/* Popular Business List */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
