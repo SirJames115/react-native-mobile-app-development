6d80b7ad89c09be15b2e1962305139f7e72d9d88
James Othniel <jamesothniel7@gmail.com>
2024-07-08 14:07:09 +0100

    added theuser intro for the profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import UserIntro from "../../components/Profile/UserIntro";
 
 export default function profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View
+      style={{
+        padding: 20,
+      }}>
+      <Text
+        style={{
+          fontFamily: "outfit-bold",
+          fontsize: 35,
+        }}>
+        profile
+      </Text>
+      {/* User info */}
+      <UserIntro />
+
+      {/* Menu List */}
     </View>
   );
 }
